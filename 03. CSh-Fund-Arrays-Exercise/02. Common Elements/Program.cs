70feb4763302e79c5bebe38e45c37c4e1202179d
P-Sht <114442816+P-Sht@users.noreply.github.com>
2023-01-06 16:18:01 +0200

    new

--- 03. CSh-Fund-Arrays-Exercise/02. Common Elements/Program.cs
+++ 03. CSh-Fund-Arrays-Exercise/02. Common Elements/Program.cs
@@ -6,7 +6,20 @@ namespace _02._Common_Elements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var one = Console.ReadLine();
+            var two = Console.ReadLine();
+
+            string[] itemsOne = one.Split();
+            string[] itemsTwo = two.Split();
+
+            for (int i = 0; i < itemsTwo.Length; i++)
+            {
+                for (int j = 0; j < itemsOne.Length; j++)
+                {
+                    if (itemsOne[j] == itemsTwo[i])
+                        Console.Write(itemsOne[j] + " "); 
+                }
+            }
         }
     }
 }
