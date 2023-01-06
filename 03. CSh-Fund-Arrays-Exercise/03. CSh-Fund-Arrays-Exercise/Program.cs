df09d8d887b359bc2b9607972cb1d398485b1471
P-Sht <114442816+P-Sht@users.noreply.github.com>
2023-01-06 14:18:04 +0200

    new 03

---
+++ 03. CSh-Fund-Arrays-Exercise/03. CSh-Fund-Arrays-Exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int[] people = new int[number];
+            int sum = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                int vagon = int.Parse(Console.ReadLine());
+                people[i] = vagon;
+                sum += vagon;
+            }
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write(people[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}
